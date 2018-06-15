85a7c3eb13303e6bfdcfeab7e9dc38b99ad4ff7b
doriandekoning <doriandekoning@gmail.com>
2018-06-15 13:43:02 +0200

    Add connection to classifier and fingerprinter

--- server.py
+++ server.py
@@ -1,19 +1,34 @@
 from flask import Flask
 from flask import json
 from flask import request
+import requests
 import base64
 app = Flask(__name__)
 
+CLASSIFIER_URL = "https://741b00e5-db75-4cab-8380-0903c0299980.mock.pstmn.io"
+FINGERPRINTER_URL = 'https://c028d11f-1a21-4d72-a5c5-f5e80b1591e1.mock.pstmn.io'
 
 @app.route("/analysis", methods=['POST'])
 def analysis():
-    #Dummy output
-    data = {'stationname':'3fm', 'song': {'name':'Colossus', 'artist':'IDLES'}, 'dj':'dorian'}
+
     # Fix base64 padding
     #Remove base64 "header"
     base64audio = request.get_json()['audio'].split(",")[1]
     # Audio in webm opus format
     audio = base64.b64decode(base64audio)
+    #TODO convert audio to wav
+
+
+    #Request to classifier
+
+    classify(audio)
+
+    songname, confidence = fingerprint(audio)
+    if confidence < 0.7:
+        return app.response_class(status=404)
+    
+    data = {'stationname':'3fm', 'song': {'name':songname, 'artist':'IDLES'}, 'dj':'dorian'}
+
     response = app.response_class(
         response =json.dumps(data),
         status=200, 
@@ -21,9 +36,27 @@ def analysis():
     )
     return response
 
+    
+def classify(audio):
+    request =  requests.get(CLASSIFIER_URL+ "/classify")
+    if request.status_code == 200 :
+        if request.json()['label'] == 'speech' :
+            return False
+        elif request.json()['label'] == 'music' :
+            return True
+
+
+def fingerprint(audio):
+    request = requests.get(FINGERPRINTER_URL + '/fingerprint')
+    if request.status_code == 200 :
+        return request.json()['songname'], request.json()['confidence']
+    return ''
+
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
+
+  de
