305dacf12f974ae4270598a8248228e4364ea6ec
doriandekoning <doriandekoning@gmail.com>
2018-06-19 10:23:05 +0200

    Add connection to fingerprinter

--- server.py
+++ server.py
@@ -3,10 +3,17 @@ from flask import json
 from flask import request
 import requests
 import base64
+import os
+import ffmpeg
 app = Flask(__name__)
 
 CLASSIFIER_URL = "https://741b00e5-db75-4cab-8380-0903c0299980.mock.pstmn.io"
-FINGERPRINTER_URL = 'https://c028d11f-1a21-4d72-a5c5-f5e80b1591e1.mock.pstmn.io'
+FINGERPRINTER_URL = 'http://mmsr-fingerprint.nl'
+
+@app.route("/health", methods=['GET'])
+def health():
+    resp = app.response_class(status=200, response =json.dumps({'status':'ok'}), mimetype='application/json')
+    return resp
 
 @app.route("/analysis", methods=['POST'])
 def analysis():
@@ -16,19 +23,18 @@ def analysis():
     base64audio = request.get_json()['audio'].split(",")[1]
     # Audio in webm opus format
     audio = base64.b64decode(base64audio)
-    #TODO convert audio to wav
 
 
     #Request to classifier
 
     classify(audio)
+    
 
     songname, confidence = fingerprint(audio)
     if confidence < 0.7:
         return app.response_class(status=404)
     
     data = {'stationname':'3fm', 'song': {'name':songname, 'artist':'IDLES'}, 'dj':'dorian'}
-
     response = app.response_class(
         response =json.dumps(data),
         status=200, 
@@ -47,17 +53,26 @@ def classify(audio):
 
 
 def fingerprint(audio):
-    file = open('audio.webm')
+    file = open('audio.webm', 'wb')
     file.write(audio)
     file.close()
-    request = requests.get(FINGERPRINTER_URL + '/fingerprint')
-    if request.status_code == 200 :
-        return request.json()['songname'], request.json()['confidence']
-    return ''
+    stream  = ffmpeg.input('audio.webm')
+    stream = ffmpeg.output(stream, 'out.wav', ac=1, acodec='pcm_s16le')
+    ffmpeg.run(stream)
+    file = open('out.wav', 'rb')
+    b = base64.b64encode(file.read())
+    file.close()
+    body = {'extension':'wav', 'file': b }
+    response = requests.post(FINGERPRINTER_URL + '/recognize', json=body)
+    if response.status_code == 200 :
+        os.remove('out.wav')
+        print( response.json()['song_name'], response.json()['confidence']/100.0)
+    return '', 0.8
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  
+    return response
